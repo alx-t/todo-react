e02bc9aefde83d09ee9ab64d6af1841d5a0260fa
Alex Tesevich <alex.tesevich@gmail.com>
2023-06-06 19:42:46 +0300

    todo с домашним заданием 3

--- src/features/model/dashboard/dashboard-selectors.ts
+++ src/features/model/dashboard/dashboard-selectors.ts
@@ -1,9 +1,14 @@
 import { createSelector } from "@reduxjs/toolkit";
-import { RootState } from "../../store/store";
+import { RootState } from "../../../store/store";
 
 export const getDashboardState = (state: RootState) => state.dashboard
 
 export const getDashbordList = createSelector(
   [getDashboardState],
   (dashboard) => [...dashboard.dashboardList].sort((a, b) => a.order - b.order)
+);
+
+export const getTasksList = createSelector(
+  getDashboardState,
+  (dashboard) => dashboard.tasks
 );
